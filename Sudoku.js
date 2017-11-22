474954e0f31345bf685303e47f5d34dc4945fca2
贺艳霞 <heyanxia1996@outlook.com>
2017-11-22 17:56:55 +0800

    'Sudoku'

--- Sudoku.js
+++ Sudoku.js
@@ -9,11 +9,11 @@ for(var i = 0; i < 9; i++) {
 
 var generateNum = function () {
 	return Math.floor(Math.random() * 9 + 1);
-}
+};
 
 var generateIndex = function () {
 	return Math.floor(Math.random() * 9);	
-}
+};
 
 var init = function () {
 	for(var i = 0; i < 9; i ++) {
@@ -26,7 +26,7 @@ var init = function () {
 		arr[0][i] = arr[0][j];
 		arr[0][j] = t;
 	}
-} 
+};
 
 var getArr = false;
 var fill = function(n) {
@@ -56,7 +56,7 @@ var fill = function(n) {
 		}
 		arr[row][col] = 0;
 	}
-}
+};
 
 var check = function(row, col) {
 	var t, curData = arr[row][col];
@@ -81,12 +81,59 @@ var check = function(row, col) {
 	return true;
 }
 
+var gameCheck = function(n, curData, blankArr) {
+	var row = Math.floor(n / 9), col = n % 9;
+	var t;
+	for(t = 0; t < 9; t++) {
+		// }
+		if (t != col && blankArr[row][t] == curData) {
+			console.log('行',row, t)
+			return {
+				result:false,
+				row: row,
+				col: t,
+			};
+		} else if(t != row && blankArr[t][col] == curData) {
+			console.log('列', t, col);
+			return {
+				result:false,
+				row: t,
+				col: col
+			};
+		}
+	}
+
+	var gongRow = Math.floor(row / 3) * 3;
+	var gongCol = Math.floor(col / 3) * 3;
+	var index = 3*(row % 3) + col % 3;
+	var newArr = [], i, j, times = 0;
+	for (i = gongRow; i < gongRow + 3; i++) {
+		for(j = gongCol; j < gongCol + 3; j++) {
+			if(blankArr[i][j] === 0) continue;
+			if((times != index) && (blankArr[i][j] == curData)) {
+				console.log('宫', i, j);
+				return {
+					result:false,
+					row: i,
+					col: j
+				};
+			}
+			times++;
+		}
+	}
+	return {
+		result:true,
+		row: t,
+		col: col
+	};
+};
+
 var newSudoku = function () {
 	init();
 	fill(9);
-}
+};
 
-var mackBlank = function () {
+var mackBlank = function (degree) {
 	var blankArr = []
 	for(var i = 0; i < 9; i++) {
 		blankArr[i] = [];
@@ -96,7 +143,7 @@ var mackBlank = function () {
 	}
 	for(var i = 0; i < 9; i ++) {
 		var newArr = [];
-		for(var j = 0; j < 4; j++) {
+		for(var j = 0; j < degree; j++) {
 			var index = generateIndex();
 			while(newArr.indexOf(index) != -1) {
 				index = generateIndex();
@@ -106,9 +153,6 @@ var mackBlank = function () {
 		}
 	}
 	return blankArr;
-}
+};
 
 newSudoku();
-// console.log(result);
-// export mackBlank;
-// console.log(mackBlank());
